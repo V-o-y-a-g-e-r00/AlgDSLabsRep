807946795e66eb690c29747e40cc25b1901d776b
fmol00 <fmol00@mail.ru>
2022-02-23 22:57:36 +0300

    Получить результаты на небольших значениях при одинаковых массивах у разных функций

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -9,23 +9,26 @@ int main(int, char**) {
 
     srand(time(0));
     int seed=rand();
-//Аргументы подставлены для пошагового выполнения    
+
+//Аргументы подставлены для пошагового выполнения 
+/*
     std::cout<<"HeapSort:"<<std::endl;
     statistics<int> s1(HeapSort<int>, 5, 5, 10000, false, true, 100, 1,(char*)"HeapSortOut.txt", seed);
     std::cout<<"QuickSortHoare:"<<std::endl;
     statistics<int> s2(QuickSortHoare, 5, 5, 10000, false, true, 100, 1,(char*)"QuickSortHoareOut.txt", seed);
     std::cout<<"ShellSort:"<<std::endl;
     statistics<int> s3(ShellSort, 5, 5, 10000, false, true, 100, 1,(char*)"ShellSortOut.txt", seed);
-
-//Агрументы для массивов больших размеров
- /*   std::cout<<"HeapSort:"<<std::endl;
-    statistics<int> s1(HeapSort<int>, 10, 200, 10, false, false, 100, 1,(char*)"HeapSortOut.txt", seed);
-    std::cout<<"QuickSortHoare:"<<std::endl;
-    statistics<int> s2(QuickSortHoare, 10, 200, 10, false, false, 100, 1,(char*)"QuickSortHoareOut.txt", seed);
-    std::cout<<"ShellSort:"<<std::endl;
-    statistics<int> s3(ShellSort, 10, 200, 10, false, false, 100, 1,(char*)"ShellSortOut.txt", seed);
-    system("./PlotScript.gpi");
 */
+//Агрументы для табуляции среднего времени выполнения
+
+    std::cout<<"HeapSort:"<<std::endl;
+    statistics<int> s1(HeapSort<int>, 100, 1000, 100, false, false, 100, 10,(char*)"HeapSortOut.txt", seed);
+    std::cout<<"QuickSortHoare:"<<std::endl;
+    statistics<int> s2(QuickSortHoare, 100, 1000, 100, false, false, 100, 10,(char*)"QuickSortHoareOut.txt", seed);
+    std::cout<<"ShellSort:"<<std::endl;
+    statistics<int> s3(ShellSort, 100, 1000, 100, false, false, 100, 10,(char*)"ShellSortOut.txt", seed);
+    system("./PlotScript.gpi");
+
 
 
 /*
