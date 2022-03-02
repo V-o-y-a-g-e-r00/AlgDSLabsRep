11d915143de521ff3e446e0e15b477d74ef0d1cb
fmol00 <fmol00@mail.ru>
2022-03-02 15:20:40 +0300

    minor

--- Lab5/Func.h
+++ Lab5/Func.h
@@ -165,5 +165,36 @@ void SetVarsForScript(edges& Edges, bool IsOriented, bool IsSavePictureToFile, c
     fd<<"\"n="<<Edges.Vector.size()<<"; m="<<m<< "\"\t"<<IsOriented<<"\t"<<IsSavePictureToFile<<"\t"<<PictureName<<std::endl;
     fd.close();
 }
-
+/*template<typename T>
+void PrintMatrix(std::vector<std::vector<T>>& Matrix, const char* MatrixName)
+{
+    typename std::vector<std::vector<T>>::iterator iteri; //два итератора, для итерации по строкам и ячейкам(столбцам) .Без typename вообще не компилируется, хотя казалось бы.
+    typename std::vector<T>::iterator iterj;
+    for(iteri=Matrix.begin(); iteri!=Matrix.end(); iteri++)
+    {
+        std::cout<< MatrixName<<"["<<std::distance(Matrix.begin(), iteri) <<"]=";
+        for(iterj=iteri->begin(); iterj!=iteri->end(); iterj++)
+        {
+            std::cout<< (*iterj)<<"\t";
+        }
+        std::cout<<std::endl;
+    }
+} */
+/*template<typename T>
+void PrintMatrixToFile(std::vector<std::vector<T>>& Matrix, const char* MatrixName, const char* filename)
+{
+    std::ofstream fd(filename);
+    typename std::vector<std::vector<T>>::iterator iteri; //два итератора, для итерации по строкам и ячейкам(столбцам) .Без typename вообще не компилируется, хотя казалось бы.
+    typename std::vector<T>::iterator iterj;
+    for(iteri=Matrix.begin(); iteri!=Matrix.end(); iteri++)
+    {
+       // fd<< MatrixName<<"["<<std::distance(Matrix.begin(), iteri) <<"]=";
+        for(iterj=iteri->begin(); iterj!=iteri->end(); iterj++)
+        {
+            fd<< (*iterj)<<"\t";
+        }
+        fd<<std::endl;
+    }
+    fd.close();
+}*/
 #endif //FUNC_H_INCLUDED
--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -10,40 +10,6 @@
 #include "edges.h"
 #include "Func.h"
 
-
-/*template<typename T>
-void PrintMatrix(std::vector<std::vector<T>>& Matrix, const char* MatrixName)
-{
-    typename std::vector<std::vector<T>>::iterator iteri; //два итератора, для итерации по строкам и ячейкам(столбцам) .Без typename вообще не компилируется, хотя казалось бы.
-    typename std::vector<T>::iterator iterj;
-    for(iteri=Matrix.begin(); iteri!=Matrix.end(); iteri++)
-    {
-        std::cout<< MatrixName<<"["<<std::distance(Matrix.begin(), iteri) <<"]=";
-        for(iterj=iteri->begin(); iterj!=iteri->end(); iterj++)
-        {
-            std::cout<< (*iterj)<<"\t";
-        }
-        std::cout<<std::endl;
-    }
-} */
-/*template<typename T>
-void PrintMatrixToFile(std::vector<std::vector<T>>& Matrix, const char* MatrixName, const char* filename)
-{
-    std::ofstream fd(filename);
-    typename std::vector<std::vector<T>>::iterator iteri; //два итератора, для итерации по строкам и ячейкам(столбцам) .Без typename вообще не компилируется, хотя казалось бы.
-    typename std::vector<T>::iterator iterj;
-    for(iteri=Matrix.begin(); iteri!=Matrix.end(); iteri++)
-    {
-       // fd<< MatrixName<<"["<<std::distance(Matrix.begin(), iteri) <<"]=";
-        for(iterj=iteri->begin(); iterj!=iteri->end(); iterj++)
-        {
-            fd<< (*iterj)<<"\t";
-        }
-        fd<<std::endl;
-    }
-    fd.close();
-}*/
-
 /*bool DFSConnectivityCheck(std::vector<std::vector<int>>& AdjacencyMatrix, std::vector<vertex>& vertices) //Проверка связности графа с помощью обхода в глубину
 {
     return false;
@@ -57,7 +23,7 @@ int main(int, char**) {
     edges Edges(N);
 
 
-    GenerateAdjacencyProb(Edges, seed, 0.5, false);
+    GenerateAdjacencyProb(Edges, seed, 0.5, true);
 //    try {GenerateAdjacencyMNumberWithLoops(Edges, seed, 25);}
  /*   try {GenerateAdjacencyMNumberNoLoops(Edges, seed, 4);}
     catch(std::string str)
@@ -68,13 +34,13 @@ int main(int, char**) {
 
 //    PrintMatrix<int>(AdjacencyMatrix, "AdjacencyMatrix");
 //    PrintMatrixToFile(AdjacencyMatrix, "AdjacencyMatrix", "AdjacencyMatrixOut.txt");
-
-    Edges.SetEdgesForPlot();
-    Edges.PrintEdges(); 
-    Edges.PrintEdges("Edges.txt"); //вывод информации о ребрах для наглядности. Не используется для рисования графа
     Vertices.SetVertXYForPlot();
     Vertices.PrintVertices();
     Vertices.PrintVertices("Vertices.dat");
-    SetVarsForScript(Edges, 0, 0, "Pic1", "VarsForScript.dat"); //число вершин и ребер в файл
+    Edges.SetEdgesForPlot(); //Вывод в файл для рисования графа
+    Edges.PrintEdges(); 
+    Edges.PrintEdges("Edges.txt"); //вывод информации о ребрах для наглядности. Не используется для рисования графа
+
+    SetVarsForScript(Edges, true, false, "Pic1", "VarsForScript.dat"); //число вершин и ребер в файл
     system("./PlotGraph.bash");
 }
