8be85a2469b9eee370f5a507f2d1819551f384e0
fmol00 <fmol00@mail.ru>
2022-03-06 21:23:04 +0300

    Закончить с созданием кода для Demo Дейкстры

--- Lab5/main_Dijkstra_Demo.cpp
+++ Lab5/main_Dijkstra_Demo.cpp
@@ -33,12 +33,11 @@ int main(int, char**) {
     GenerateGraph(Graph1, 4, 100, generator1, PresentHandler);
     Graph1.ShowPlot(0, "");
 
-    PresentHandler.Mode=0;
-//    std::vector<int> ShortestDist;
+    PresentHandler.Mode=1;
     std::vector<std::vector<int>> Pathes(N);
     Dijkstra(Graph1, 0, Pathes, PresentHandler);
 
-    Graph1.ShowPlot(false, "");
+//    Graph1.ShowPlot(false, "");
     PrintMatrix(Pathes, "Pathes");
 
 }
