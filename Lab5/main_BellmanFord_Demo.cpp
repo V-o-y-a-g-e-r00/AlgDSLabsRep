5a60fa76d862fe06af4208e683e7d78466f252bd
fmol00 <fmol00@mail.ru>
2022-03-07 22:51:08 +0300

    minor

--- Lab5/main_BellmanFord_Demo.cpp
+++ Lab5/main_BellmanFord_Demo.cpp
@@ -23,19 +23,20 @@ int main(int, char**) {
     int N=5;
     Graph Graph1(N);
     Graph1.IsWithVerticesWeights=true;
-    GenerateGraph(Graph1, 4, 100, generator1, PresentHandler);
+    GenerateGraph(Graph1, 6, 100, generator1, PresentHandler);
     Graph1.ShowPlot(0, "Pic");
 
     PresentHandler.Mode=1;
     std::vector<int> x;
     std::vector<std::vector<int>> D;
     int s=0;
-    BellmanFord(Graph1, s, x, D);
+    PresentHandler.Mode=1;
+    BellmanFord(Graph1, s, x, D, PresentHandler);
 
 //    Graph1.ShowPlot(false, "");
-    PrintMatrix(D, "BellmanFord D");
+ /*   PrintMatrix(D, "BellmanFord D");
 
     std::cout <<"x=";
     for(auto& i: x) std::cout<<i<<"\t";
-    std::cout<<std::endl;
+    std::cout<<std::endl; */
 }
