9088772006b2d11df815764054da815227074d97
fmol00 <fmol00@mail.ru>
2022-04-08 01:24:20 +0300

    Загрузка матриц из файлов с комментариями и пустыми строками

--- Lab5/Graph.h
+++ Lab5/Graph.h
@@ -1,7 +1,11 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <fstream>
+#include <string>
+#include <sstream>
 #include "VisualGraph.h"
+
 
 class Graph: public VisualGraph
 {
@@ -10,6 +14,8 @@ public:
     {
     private:
         std::vector<vertex>& refVertices; //ссылка на массив вершин, находящийся в родительском объекте. Решил сделать закрытой, т.к. обращение к одной сущности по двум именам не есть гуд.
+    //    void getlineWithCheck()
+    
     public:
         vertices(std::vector<vertex>& NrefVertices): refVertices(NrefVertices)
         {
@@ -33,6 +39,57 @@ public:
         {
             for(auto& i: refEdges)
                 for(auto& j: i) j.Color=1;
+        }
+        void AdjacencyFromFile(char *filename)
+        {
+            std::ifstream fin(filename);
+            if(!fin.is_open()) //проверка на успешность открытия файла
+            {
+                std::stringstream ss;
+                ss << "Can not open file:"<<filename<<std::endl;
+                throw(ss.str());
+            }
+            for(int i=0; i<refEdges.size(); i++)
+            {
+                std::string str; //Строка из файла
+                bool IsSatisf=false; //fasle строку нужно пропустить. Т.е. не пускать её в данные.
+                do{
+                    
+                    if(std::getline(fin, str)) //чтение прошло успешно //getline возвращает указатель потока. Указатель потока при перегрузке в bool в "хорошем" состоянии перегружается в true в "плохом" в false. Т.е. если чтение было удачным, то вернется true
+                    {
+                        if(str=="")
+                        {
+                            std::cout<<"AdjacencyFromFile: Unexpected empty string in file. Do you sure about your array size in file? The data will be read anyway"<<std::endl;
+                            IsSatisf=false;
+                        }
+                        else if(str.at(0)=='#')
+                        {
+                            IsSatisf=false;
+                        }
+                        else IsSatisf=true;
+                    }
+            //    std::cout<<"fin.good()="<<fin.good()<<"!IsSatisf="<<!IsSatisf<<std::endl;
+                }while(fin.good() && !IsSatisf);
+                if(!fin.good()&&!IsSatisf)
+                {
+                    if(fin.eof())
+                    {
+                        std::cout<<"AdjacencyFromFile: Unexpected end of file. Do you sure about your array size in file? The data will be read anyway"<<std::endl;
+                    }
+                    else
+                    {
+                        std::cout<<"AdjacencyFromFile: Unexpected error"<<std::endl;
+                    }
+                    break; //выходим из итеративного цикла
+                }
+                std::cout<<"str="<<str<<std::endl;
+
+            //    for(int j=0; j<refEdges.size(); j++)
+                {
+                    
+                }
+            }
+            fin.close();
         }
 
     };
---
+++ Lab5/main_FromFileDebug.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Graph.h"
+
+
+int main(int, char**) {
+Graph Graph1(4);
+Graph1.edgesEdges.AdjacencyFromFile((char *)"AdjM.txt");
+//Graph1.PrintEdges();
+return 0;
+}
