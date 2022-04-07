60a476eeb356846ca83858ccf9636fe98d756d6b
fmol00 <fmol00@mail.ru>
2022-04-08 02:47:58 +0300

    minor

--- Lab5/Graph.h
+++ Lab5/Graph.h
@@ -14,7 +14,41 @@ public:
     {
     private:
         std::vector<vertex>& refVertices; //ссылка на массив вершин, находящийся в родительском объекте. Решил сделать закрытой, т.к. обращение к одной сущности по двум именам не есть гуд.
-    //    void getlineWithCheck()
+        bool getstr(std::ifstream& fin, std::string& str) //true все хорошо false нужно выйти из цикла
+        {
+            bool IsSatisf=false; //fasle строку нужно пропустить. Т.е. не пускать её в данные.
+            do{
+                
+                if(std::getline(fin, str)) //чтение прошло успешно //getline возвращает указатель потока. Указатель потока при перегрузке в bool в "хорошем" состоянии перегружается в true в "плохом" в false. Т.е. если чтение было удачным, то вернется true
+                {
+                    if(str=="")
+                    {
+                        std::cout<<"xyFromFile: Unexpected empty string in file. Do you sure about your array size in file? The data will be read anyway"<<std::endl;
+                        IsSatisf=false;
+                    }
+                    else if(str.at(0)=='#')
+                    {
+                        IsSatisf=false;
+                    }
+                    else IsSatisf=true;
+                }
+        //    std::cout<<"fin.good()="<<fin.good()<<"!IsSatisf="<<!IsSatisf<<std::endl;
+            }while(fin.good() && !IsSatisf);
+            if(!fin.good()&&!IsSatisf)
+            {
+                if(fin.eof())
+                {
+                    std::cout<<"FromFile: Unexpected end of file. Do you sure about your array size in file? The data will be read anyway"<<std::endl;
+                }
+                else
+                {
+                    std::cout<<"FromFile: Unexpected error"<<std::endl;
+                }
+                return false; //выходим из итеративного цикла
+            }
+            std::cout<<"str="<<str<<std::endl;
+            return true;
+        }
     
     public:
         vertices(std::vector<vertex>& NrefVertices): refVertices(NrefVertices)
@@ -25,15 +59,67 @@ public:
         {
             for(auto& i: refVertices) i.Color=11;
         }
+        void xyFromFile(char *filename)
+        {
+            std::ifstream fin(filename);
+            if(!fin.is_open()) //проверка на успешность открытия файла
+            {
+                std::stringstream ss;
+                ss << "Can not open file:"<<filename<<std::endl;
+                throw(ss.str());
+            }
+            for(int i=0; i<refVertices.size(); i++)
+            {
+                std::string str; //Строка из файла
+                if(!getstr(fin, str)) break;
+
+                std::stringstream ss;
+                ss<<str;
+                ss>>refVertices.at(i).x;
+                ss>>refVertices.at(i).y;
+            }
+            fin.close();
+        }
     };
     class edges
     {
     private:
         std::vector<std::vector<edge>>& refEdges;
-/*        std::istream& getlineCheck() //возвр тип как у std::getline()
+        bool getstr(std::ifstream& fin, std::string& str) //true все хорошо false нужно выйти из цикла
         {
-
-        }*/
+            bool IsSatisf=false; //fasle строку нужно пропустить. Т.е. не пускать её в данные.
+            do{
+                
+                if(std::getline(fin, str)) //чтение прошло успешно //getline возвращает указатель потока. Указатель потока при перегрузке в bool в "хорошем" состоянии перегружается в true в "плохом" в false. Т.е. если чтение было удачным, то вернется true
+                {
+                    if(str=="")
+                    {
+                        std::cout<<"AdjacencyFromFile: Unexpected empty string in file. Do you sure about your array size in file? The data will be read anyway"<<std::endl;
+                        IsSatisf=false;
+                    }
+                    else if(str.at(0)=='#')
+                    {
+                        IsSatisf=false;
+                    }
+                    else IsSatisf=true;
+                }
+        //    std::cout<<"fin.good()="<<fin.good()<<"!IsSatisf="<<!IsSatisf<<std::endl;
+            }while(fin.good() && !IsSatisf);
+            if(!fin.good()&&!IsSatisf)
+            {
+                if(fin.eof())
+                {
+                    std::cout<<"FromFile: Unexpected end of file. Do you sure about your array size in file? The data will be read anyway"<<std::endl;
+                }
+                else
+                {
+                    std::cout<<"FromFile: Unexpected error"<<std::endl;
+                }
+                return false; //выходим из итеративного цикла
+            }
+            std::cout<<"str="<<str<<std::endl;
+            return true;
+        }
     public:
         edges(std::vector<std::vector<edge>>& NrefEdges): refEdges(NrefEdges)
         {
@@ -56,38 +142,7 @@ public:
             for(int i=0; i<refEdges.size(); i++)
             {
                 std::string str; //Строка из файла
-                bool IsSatisf=false; //fasle строку нужно пропустить. Т.е. не пускать её в данные.
-                do{
-                    
-                    if(std::getline(fin, str)) //чтение прошло успешно //getline возвращает указатель потока. Указатель потока при перегрузке в bool в "хорошем" состоянии перегружается в true в "плохом" в false. Т.е. если чтение было удачным, то вернется true
-                    {
-                        if(str=="")
-                        {
-                            std::cout<<"AdjacencyFromFile: Unexpected empty string in file. Do you sure about your array size in file? The data will be read anyway"<<std::endl;
-                            IsSatisf=false;
-                        }
-                        else if(str.at(0)=='#')
-                        {
-                            IsSatisf=false;
-                        }
-                        else IsSatisf=true;
-                    }
-            //    std::cout<<"fin.good()="<<fin.good()<<"!IsSatisf="<<!IsSatisf<<std::endl;
-                }while(fin.good() && !IsSatisf);
-                if(!fin.good()&&!IsSatisf)
-                {
-                    if(fin.eof())
-                    {
-                        std::cout<<"AdjacencyFromFile: Unexpected end of file. Do you sure about your array size in file? The data will be read anyway"<<std::endl;
-                    }
-                    else
-                    {
-                        std::cout<<"AdjacencyFromFile: Unexpected error"<<std::endl;
-                    }
-                    break; //выходим из итеративного цикла
-                }
-                std::cout<<"str="<<str<<std::endl;
-
+                if(!getstr(fin, str)) break;
 
                 std::stringstream ss;
                 ss<<str;
@@ -98,7 +153,29 @@ public:
             }
             fin.close();
         }
+        void WeightFromFile(char *filename)
+        {
+            std::ifstream fin(filename);
+            if(!fin.is_open()) //проверка на успешность открытия файла
+            {
+                std::stringstream ss;
+                ss << "Can not open file:"<<filename<<std::endl;
+                throw(ss.str());
+            }
+            for(int i=0; i<refEdges.size(); i++)
+            {
+                std::string str; //Строка из файла
+                if(!getstr(fin, str)) break;
 
+                std::stringstream ss;
+                ss<<str;
+                for(int j=0; j<refEdges.size(); j++)
+                {
+                    ss>>refEdges.at(i).at(j).Weight;
+                }
+            }
+            fin.close();
+        }
     };
 
     vertices vertsVertices; //обертки
--- Lab5/main_FromFileDebug.cpp
+++ Lab5/main_FromFileDebug.cpp
@@ -4,7 +4,10 @@
 
 int main(int, char**) {
 Graph Graph1(4);
-Graph1.edgesEdges.AdjacencyFromFile((char *)"AdjM.txt");
-Graph1.PrintEdges();
+Graph1.Edges.at(3).at(3).Adjacency=-1;
+//Graph1.edgesEdges.AdjacencyFromFile((char *)"AdjM.txt");
+//Graph1.PrintEdges();
+Graph1.vertsVertices.xyFromFile((char *)"xyVert.txt");
+Graph1.PrintVertices();
 return 0;
 }
