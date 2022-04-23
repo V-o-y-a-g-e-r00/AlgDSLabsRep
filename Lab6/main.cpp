8b2ed75b48fc123ead8a086e82b06da7ced78ca0
fmol00 <fmol00@mail.ru>
2022-04-23 19:19:28 +0300

    minor

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -8,6 +8,22 @@
 #define EMPTYCELL '.'
 #define CORNER '+'
 
+#define HORIZWALL "\u2500" // '-'
+#define VERTWALL "\u2502" // '|'
+
+#define CORTL "\u250C" // 'Г'
+#define CORTR "\u2510" // '~.'
+#define CORBL "\u2514" // 'L'
+#define CORBR "\u2518" // '_|'
+
+#define TOPWALL "\u252C" // '~.Г'
+#define BOTTOMWALL "\u2534" // '_L'
+#define LWALL "\u251C" // '|-'
+#define RWALL "\u2524" // '-|'
+
+#define FULLWALL "\u253C" // '-|-'
+#define DOT "." //для случаев, когда нет всех четырех стен
+
 class maze
 {
 private:
@@ -44,6 +60,13 @@ public:
         int dj=((alpha-1)%2)*(((alpha-1)%4)*((alpha-1)%2)-2);
         (HasWall)? BaseVector.at(i*2+1+di).at(j*2+1+dj)=WALL : BaseVector.at(i*2+1+di).at(j*2+1+dj)=NOWALL;
     }
+    bool HasWall(int i, int j, int alpha) //прикол в том, что мы можем узнать, существуют ли стены у ячейки, даже если самой ячейки не существует. Главное, чтобы сами стены сущестововали в лабиринте.
+    {
+        int di=(alpha%2)*((alpha%4)*(alpha%2)-2); //формулы получены из графиков
+        int dj=((alpha-1)%2)*(((alpha-1)%4)*((alpha-1)%2)-2);
+        if(BaseVector.at(i*2+1+di).at(j*2+1+dj)==WALL) return true;
+        else return false;
+    }
     void Show(char* filename=(char*)"cin")
     {
         std::stringstream ss;
@@ -52,6 +75,97 @@ public:
             for(auto& j: i)
             {
                 ss<<j;
+            }
+            ss<<std::endl;
+        }
+        std::cout<<ss.str();
+    }
+    void CharFromWallFlags(std::stringstream& ss, std::string& WallFlags) //Вспомогательная функция для получения символа по флагу, т.е. по его описанию.
+    {
+        switch (std::stoi(WallFlags))
+        {
+        case 0000:
+            ss<<DOT;
+            break;
+        case 0001:
+            ss<<HORIZWALL;
+            break;
+        case 0010:
+            ss<<VERTWALL;
+            break;
+        case 0011:
+            ss<<CORBL;
+            break;
+        case 0100:
+            ss<<HORIZWALL;
+            break;
+        case 0101:
+            ss<<HORIZWALL;
+            break;
+        case 0110:
+            ss<<CORBR;
+            break;
+        case 0111:
+            ss<<BOTTOMWALL;
+            break;
+        case 1000:
+            ss<<VERTWALL;
+            break;
+        case 1001:
+            ss<<CORTL;
+            break;
+        case 1010:
+            ss<<VERTWALL;
+            break;
+        case 1011:
+            ss<<LWALL;
+            break;
+        case 1100:
+            ss<<CORTR;
+            break;
+        case 1101:
+            ss<<TOPWALL;
+            break;
+        case 1110:
+            ss<<RWALL;
+            break;
+        case 1111:
+            ss<<FULLWALL;
+            break;
+        default:
+            break;
+        }
+    }
+    void ShowDecorate(char* filename=(char*)"cin")
+    {
+        std::stringstream ss;
+        std::string str;
+    //    ss<<HORIZWALL<<VERTWALL<<CORTL<<CORTW<<CORBL<<CORBR<<TOPWALL<<BOTTOMWALL<<LWALL<<RWALL<<FULLWALL<< std::endl; //проверка отображения символов.
+    //    std::cout<<ss.str();
+        std::string WallFlags;
+
+
+        for(int i=0; i<n; i++)
+        {
+            // Тут просто корректируем граничные условия
+            WallFlags.clear();
+            if(HasWall(i,0, 3)) WallFlags.insert(0,"1"); else WallFlags.insert(0,"0");
+            if(HasWall(i,0, 2)) WallFlags.insert(0,"1"); else WallFlags.insert(0,"0");
+            WallFlags.insert(0,"0"); //слева ячеек нет, и значит стен, уходящих влево тоже нет
+            if(i+1<n) if(HasWall(i+1,0, 2)) WallFlags.insert(0,"1"); else WallFlags.insert(0,"0"); 
+            CharFromWallFlags(ss, WallFlags);
+            // /////   
+            for(int j=0; j<m; j++)
+            {
+                ss<<HORIZWALL;
+                WallFlags.clear();
+                if ((i+1<n)&&(j+1<m)) if(HasWall(i+1,j+1, 1)) WallFlags.insert(0,"1"); else WallFlags.insert(0,"0"); else WallFlags.insert(0,"1");
+                if(HasWall(i,j, 0)) WallFlags.insert(0,"1"); else WallFlags.insert(0,"0");
+                if(HasWall(i,j, 3)) WallFlags.insert(0,"1"); else WallFlags.insert(0,"0");
+                if ((i+1<n)&&(j+1<m)) if(HasWall(i+1,j+1, 2)) WallFlags.insert(0,"1"); else WallFlags.insert(0,"0"); else WallFlags.insert(0,"1");
+
+                CharFromWallFlags(ss, WallFlags);
+                
             }
             ss<<std::endl;
         }
@@ -101,10 +215,13 @@ void MazeFromFile(maze& Maze, char* filename)
 
 
 }
-
 int main(int, char**) {
     std::cout << "Hello, world!"<<(-1)%2<<std::endl;
     maze Maze(3,3);
     Maze.SetCellWalls(0,0,0,false);
     Maze.Show();
+  //  std::string str = "\u256C\u265E";
+// std::cout << str << std::endl;
+    std::cout << "------------" << std::endl;
+    Maze.ShowDecorate();
 }
