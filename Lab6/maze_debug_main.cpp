87b9bd6405674eb9b878339c82219a7b8b92ad7d
fmol00 <fmol00@mail.ru>
2022-04-25 21:58:46 +0300

    minor

--- Lab6/maze_debug_main.cpp
+++ Lab6/maze_debug_main.cpp
@@ -11,7 +11,18 @@ int main(int, char**) {
 //    std::default_random_engine generator1(seed);
 
     maze Maze(10,10);
-    Maze.SetCellWalls(0,0, 3, false);
+    for(int i=0; i<Maze.n; i++)
+    {
+        for(int j=0; j<Maze.m; j++)
+        {
+            Maze.SetCellWalls(i,j, 0, false);
+            Maze.SetCellWalls(i,j, 3, false);
+        }
+    }
+ //   Maze.SetCellWalls(0,0, 0, false);
+ //   Maze.SetCellWalls(0,0, 3, false);
+//    Maze.SetCellWalls(1,1, 1, false);
+ //   Maze.SetCellWalls(1,1, 2, false);
  /*   try
     {
          MazeFromFile(Maze, (char*)"Maze.txt");
