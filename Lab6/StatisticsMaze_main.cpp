a194da76ab9b709de1ba32df5d5e6cbe88ae51ae
fmol00 <fmol00@mail.ru>
2022-04-25 20:43:44 +0300

    Получить результаты времени выполнения алг-ов ОлдосБродер Вилсон ВилсонПоследовательный Двоичное древо

--- Lab6/StatisticsMaze_main.cpp
+++ Lab6/StatisticsMaze_main.cpp
@@ -15,14 +15,14 @@ int main(int, char**) {
     std::default_random_engine generator1(seed);
 
     presenthandler PrHandler;
-    PrHandler.Mode=11;
+    PrHandler.Mode=0; //11 выводить генерируемые лабиринты
     plothandler PlotHandler;
 
-    int NStart=4, NEnd=6, NStep=1, NumberOfRun=1;
+    int NStart=4, NEnd=50, NStep=5, NumberOfRun=100;
     double MRatio=1;
-    StatisticsMaze(NStart, NEnd, NStep, MRatio, NumberOfRun, generator1, PrHandler, PlotHandler.addandreturn("AldousBroderOut.txt"), AldousBroder);
-    StatisticsMaze(NStart, NEnd, NStep, MRatio, NumberOfRun, generator1, PrHandler, PlotHandler.addandreturn("WilsonOut.txt"), Wilson);
+    StatisticsMaze(NStart, NEnd, NStep, MRatio, NumberOfRun, generator1, PrHandler, PlotHandler.addandreturn("WilsonOut.txt"), Wilson); //200x200 около 9 секунд генерируются
     StatisticsMaze(NStart, NEnd, NStep, MRatio, NumberOfRun, generator1, PrHandler, PlotHandler.addandreturn("WilsonSerialOut.txt"), WilsonSerial);
+    StatisticsMaze(NStart, NEnd, NStep, MRatio, NumberOfRun, generator1, PrHandler, PlotHandler.addandreturn("AldousBroderOut.txt"), AldousBroder);
     int alpha=0; //угол, куда смотрит вырез стены
     StatisticsMaze(NStart, NEnd, NStep, MRatio, NumberOfRun, generator1, PrHandler, PlotHandler.addandreturn("BinaryTreeOut.txt"), BinaryTree, alpha);
     
