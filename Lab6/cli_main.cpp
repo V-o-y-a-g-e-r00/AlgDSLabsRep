7754d96ed9263e9f2c50c8fdc72fe69f863151c2
fmol00 <fmol00@mail.ru>
2022-05-08 22:13:41 +0300

    Проверка символов на цифры с помощью потока ввода std::cin

--- Lab6/cli_main.cpp
+++ Lab6/cli_main.cpp
@@ -10,18 +10,39 @@
 
 int main(int, char**) {
 
-std::cin.sync_with_stdio(false);
-
-std::cin.clear();
-std::cout<<"std::cin.rdbuf()->in_avail()="<<std::cin.rdbuf()->in_avail()<<std::endl;
-std::cin.ignore(std::cin.rdbuf()->in_avail()-1);
+std::cin.sync_with_stdio(false); //Синхронизируем с вводом stdio. Без этого не работает in_avail()
 
 std::string str;
-std::cout<<"Ввод:"<<std::endl;
-std::cin>>str;
-std::cout<<"str="<<str<<std::endl;
+bool IsGood=false;
+while(!IsGood)
+{
+    //Вводим используя стандартный поток ввода. (мерзкая штука)
+    std::cin.clear(); //Очищаем флаги
+    std::cin.ignore(std::cin.rdbuf()->in_avail()-1); //Пропускаем все символы в буфере
+
+    std::cout<<"Ввод:"<<std::endl;
+    std::cin>>str;
+    std::cout<<"str="<<str<<std::endl;
+
+    IsGood=true;
+    //Проверяем на недопустимые симоволы
+    if(str.find_first_not_of("0123456789")!=std::string::npos)
+    {
+        IsGood=false;
+    }
+    //Проверяем на лишние символы в потоке
+        
+//    std::cout<<"before isspace"<<std::endl;
+    while(std::isspace(std::cin.peek()) && std::cin.rdbuf()->in_avail()-1!=0) std::cin.ignore(1); //отбрасываем whitespace -ы
+//    std::cout<<"after isspace"<<std::endl;
+    if(std::cin.rdbuf()->in_avail()-1!=0)
+    {
+        IsGood=false;
+    }
+}
 
 
+/*
 
 
 std::cin.clear();
@@ -33,6 +54,6 @@ std::cout<<"Ввод:"<<std::endl;
 std::cin>>str;
 std::cout<<"str="<<str<<std::endl;
 std::cout<<"std::cin.rdbuf()->in_avail()="<<std::cin.rdbuf()->in_avail()<<std::endl;
-
+*/
 return 0;
 }
